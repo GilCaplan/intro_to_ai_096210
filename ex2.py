f972ec5c60306d711e5d16cfdfca0ec5c7f1387b
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2024-12-24 16:33:31 +0200

    starting, added files

---
+++ ex2.py
@@ -0,0 +1,14 @@
+
+ids = ['111111111',]
+
+class GringottsController:
+
+    def __init__(self, map_shape, harry_loc, initial_observations):
+        pass
+        # TODO: fill in
+        # Timeout: 60 seconds
+
+    def get_next_action(self, observations):
+        pass
+        # TODO: fill in
+        # Timeout: 5 seconds
